7328649a16b23a3e5324eb7c9c4481b29d31dcc7
Gautam Mahato <150373600+Gautam-07@users.noreply.github.com>
2025-08-19 23:11:07 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://byteMonk:byteMonk30@cluster0.n4vappq.mongodb.net/food-del';
+        const mongoURI = process.env.MONGODB_URI || ' ';
         await mongoose.connect(mongoURI);
         console.log("MongoDB connected successfully");
     } catch (error) {
